f22a523f7a0042191df3e4edd47f0e6054fb26dd
Bogdan Despotov <despotov@gmail.com>
2020-06-28 20:51:36 +0300

    Adding an LCD screen and a DHT22 sensor

--- humid.ino
+++ humid.ino
@@ -1,12 +1,14 @@
 #include "DHT.h"
+#include "LiquidCrystal.h"
 
 #define DHTPIN 8
-#define DHTTYPE DHT11
+#define DHTTYPE DHT22
 #define HUMIDIFIER 7
 #define HEAT 6
 #define ON HIGH
 #define OFF LOW
 
+LiquidCrystal LCD(12, 11, 5, 4, 3, 2);
 
 DHT dht(DHTPIN, DHTTYPE);
 
@@ -15,12 +17,12 @@ void switch_relay(int relay, int state) {
 }
 
 void operate_device(int ref, int lower, int higher, int device) {
-  if(ref < lower & digitalRead(device) != ON) {
+  if (ref < lower & digitalRead(device) != ON) {
     switch_relay(device, ON);
     Serial.print("Switching on device on pin:\t");
     Serial.print(device);
   }
-  if(ref > higher & digitalRead(device) != OFF) {
+  if (ref > higher & digitalRead(device) != OFF) {
     switch_relay(device, OFF);
     Serial.print("Switching off device on pin: \t");
     Serial.print(device);
@@ -30,8 +32,9 @@ void operate_device(int ref, int lower, int higher, int device) {
 void setup() {
   Serial.begin(9600);
   Serial.println(F("Starting incubator test!"));
-
   dht.begin();
+  LCD.begin(16, 2);
+  LCD.print("Booting...");
   pinMode(HUMIDIFIER, OUTPUT);
   pinMode(HEAT, OUTPUT);
 
@@ -48,12 +51,23 @@ void loop() {
     return;
   }
 
-  operate_device(h, 80, 91, HUMIDIFIER);
-  operate_device(t, 22, 25, HEAT);
 
   Serial.print(F("Humidity: "));
+  LCD.setCursor(0, 1);
+  LCD.clear();
+  LCD.print("Humidity: ");
+  LCD.setCursor(10, 0);
+  LCD.print(h);
+  LCD.setCursor(15, 0);
+  LCD.print('%');
+  LCD.setCursor(0, 2);
   Serial.print(h);
   Serial.print(F("%  Temperature: "));
+  LCD.print("Temp: ");
+  LCD.setCursor(6, 2);
+  LCD.print(t);
+  LCD.setCursor(12, 2);
+  LCD.print("C");
   Serial.print(t);
   Serial.print(F("°C "));
   Serial.print("\n");
