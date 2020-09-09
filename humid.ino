3894181e0935a9174f3f080d750c4c69054f9edf
Bogdan Despotov <despotov@gmail.com>
2020-09-09 20:47:39 +0300

    Adding a periodic action method

--- humid.ino
+++ humid.ino
@@ -8,9 +8,14 @@
 #define ON HIGH
 #define OFF LOW
 
+unsigned long startMillis;
+unsigned long currentMillis;
+
 LiquidCrystal LCD(12, 11, 5, 4, 3, 2);
 
 DHT dht(DHTPIN, DHTTYPE);
+
+boolean activated;
 
 void switch_relay(int relay, int state) {
   digitalWrite(relay, state);
@@ -29,27 +34,48 @@ void operate_device(int ref, int lower, int higher, int device) {
   }
 }
 
+
+void periodic_action(int device, unsigned long period, unsigned long duration) {
+  currentMillis = millis();
+  if (activated == true && (currentMillis - startMillis >= duration)) {
+    switch_relay(device, OFF);
+    Serial.println(F("Switching device off"));
+    activated = false;
+  }
+  else if (activated == false && (currentMillis - startMillis >= period)) {
+    Serial.println(F("Switching device on"));
+    switch_relay(device, ON);
+    startMillis = currentMillis;
+    activated = true;
+  }
+}
+
 void setup() {
   Serial.begin(9600);
-  Serial.println(F("Starting incubator test!"));
+  Serial.println(F("Starting incubator debug!"));
   dht.begin();
   LCD.begin(16, 2);
   LCD.print("Booting...");
   pinMode(HUMIDIFIER, OUTPUT);
   pinMode(HEAT, OUTPUT);
-
+  startMillis = millis();
 }
 
 void loop() {
-  delay(5000);
+  delay(1000);
 
   float h = dht.readHumidity();
   float t = dht.readTemperature();
 
   if (isnan(h) || isnan(t)) {
     Serial.println(F("Failed to read from DHT sensor!"));
+    LCD.setCursor(0, 1);
+    LCD.clear();
+    LCD.print("Fail read sensor");
     return;
   }
+
+  periodic_action(HUMIDIFIER, 30000, 10000);
 
 
   Serial.print(F("Humidity: "));
